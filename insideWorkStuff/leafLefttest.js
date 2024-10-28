5e57e33b45ccea11fab6f11414d13e9da8e804d8
Adam Fehse <143850161+adamFougasse@users.noreply.github.com>
2024-10-28 13:26:13 -0700

    adding data next, side bar kdina done

--- insideWorkStuff/leafLefttest.js
+++ insideWorkStuff/leafLefttest.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([32.216868, -110.974298], 9);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
+    maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
--- insideWorkStuff/sideBarTest.js
+++ insideWorkStuff/sideBarTest.js
@@ -3,6 +3,6 @@ function showContent(category, keyword) {
     const contentDiv = document.getElementById('mainContent');
     contentDiv.innerHTML = `
             <h6 class="text-center">${category}</h6>
-            <p class="text-center">You clicked on Keyword ${keyword} from the ${category} category.</p>
+            <p class="text-center"> ${keyword} </p>
         `;
 }
