3f2609a1d9e8f4fc01cd57c4d5f29ea0ea6427e3
Adam Fehse <143850161+adamFougasse@users.noreply.github.com>
2024-10-28 11:14:58 -0700

    lolok

--- insideWorkStuff/sideBarTest.js
+++ insideWorkStuff/sideBarTest.js
@@ -2,7 +2,7 @@
 function showContent(category, keyword) {
     const contentDiv = document.getElementById('mainContent');
     contentDiv.innerHTML = `
-            <h3 class="text-center">${category}</h3>
+            <h6 class="text-center">${category}</h6>
             <p class="text-center">You clicked on Keyword ${keyword} from the ${category} category.</p>
         `;
 }
